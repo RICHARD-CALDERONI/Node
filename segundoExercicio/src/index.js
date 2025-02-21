8753ddd19f859452623c5e95306ccf10a91e83e4
Richard Lima Calderoni <richard.calderoni@hotmail.com>
2025-02-21 16:50:39 -0300

    implementando funcao verificaPalavrasDuplicadas()

--- segundoExercicio/src/index.js
+++ segundoExercicio/src/index.js
@@ -2,10 +2,23 @@ const fs = require('fs');
 
 const caminhoArquivo = process.argv[2];
 
-fs.readFile(caminhoArquivo, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
+fs.readFile(caminhoArquivo, 'utf8', (erro, texto) => {
+  if (erro) {
+    console.error(erro);
     return;
   }
-  console.log(data);
+  verificaPalavrasDuplicadas(texto);
 });
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  listaPalavras.forEach(palavra => {
+    if (resultado[palavra]) {
+      resultado[palavra]++;
+    } else {
+      resultado[palavra] = 1;
+    }
+  });
+  console.log(resultado)
+}
